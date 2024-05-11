e30d6b67f363fae11edb6b7f65e2832b651025c6
liaotenghui <2104901558@qq.com>
2024-05-11 21:12:00 +0800

    收藏网站

---
+++ mcwdist/dist/js/data.js
@@ -0,0 +1,52 @@
+export default {
+   pData:{
+    course:[
+        {name:'菜鸟教程',url:'https://www.runoob.com/'},
+        {name:'Jquery之家',url:'http://www.htmleaf.com/'},
+        {name:'正则表达式',url:'http://web.h3399.cn/Regular_Expressions.htm'},
+        {name:'正则匹配',url:'http://tool.oschina.net/regex'},
+		{name:'小程序',url:'https://developers.weixin.qq.com/miniprogram/dev/reference/configuration/app.html'},
+        {name:'正则匹配',url:'http://tool.oschina.net/regex'},
+        {name:'express生成器',url:'https://www.expressjs.com.cn/starter/generator.html'},
+    ],
+    frame:[
+        {name:'Bootstrap4',url:'https://v4.bootcss.com/docs/getting-started/download/'},
+        {name:'Jquery插件库',url:'https://www.jq22.com/'},
+        {name:'fullPage.js',url:'https://www.dowebok.com/demo/2014/77/'},
+		{name:'lodash(深拷贝js)',url:'https://www.lodashjs.com/'},
+		{name:'Swiper.js',url:'https://www.swiper.com.cn/'},
+		{name:'Nodejs',url:'https://nodejs.cn/'},
+		{name:'Vue.js',url:'https://cn.vuejs.org/'},
+        {name:'laravel8',url:'https://learnku.com/docs/laravel/8.x'},
+        {name:'animate.css',url:'https://animate.style/'},
+        {name:'Element',url:'https://element.eleme.cn/#/zh-CN'},
+        {name:'eCharts',url:'https://echarts.apache.org/zh/index.html'},
+        {name:'React',url:'https://react.docschina.org/'},
+        {name:'eCharts',url:'https://echarts.apache.org/zh/index.html'},
+    ],
+    forum:[
+        {name:'CSDN',url:'https://download.csdn.net/?utm_source=csdn_dm_tb&spm=1003.2018.3001.7949'},
+		{name:'MSDN',url:'https://developer.mozilla.org/zh-CN/docs/Web/JavaScript'},
+        {name:'bootCDN',url:'https://www.bootcdn.cn/'},
+        {name:'gitHub',url:'https://www.bootcdn.cn/'},
+    ]
+   },
+    sData:[
+    {name:'智慧教育平台',url:'https://www.cqooc.com/'},
+    {name:'CET-4/6',url:'https://passport.neea.edu.cn/Login'},
+    {name:'智慧职教',url:'https://mooc.icve.com.cn/cms/index.htm'},
+    {name:'学习通',url:'https://i.chaoxing.com/base?t=1702540604577'},
+    {name:'智慧职教',url:'https://mooc.icve.com.cn/cms/index.htm'},
+    
+],tData:[
+    {name:'RGB',url:'https://tool.oschina.net/commons?type=3'},
+    {name:'PPT模板',url:'https://www.1ppt.com/moban/zhiwu/'},
+    {name:'系统下载',url:'https://msdn.itellyou.cn/'},
+    {name:'文心一言',url:'https://msdn.itellyou.cn/'},
+    {name:'waifu2x',url:'https://waifu2x.udp.jp/'},
+    {name:'极简壁纸',url:'https://bz.zzzmh.cn/index'},
+    {name:'MyFreeMp3',url:'http://tools.liumingye.cn/music/#/search/M/song/one%20chance'},
+    
+]
+
+}
